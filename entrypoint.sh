6e2100e72aed39a3d9994cfc48f4cfee4499adb4
cristina-grosu <cristina.grosu@bigstep.com>
2016-09-04 23:08:09 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,19 @@ elif [ "$HOSTNAME" = "" ]; then
   	sed "s/HOSTNAME/$HOSTNAME/" /opt/hadoop/etc/hadoop/core-site.xml.template >> /opt/hadoop/etc/hadoop/core-site.xml
 fi
 
-if ["$DATALAKE_USER" != ""]; then
+if [ "$DATALAKE_USER" != "" ]; then
 	sed "s/DATALAKE_USER/$DATALAKE_USER/" /opt/hadoop/etc/hadoop/core-site.xml >> /opt/hadoop/etc/hadoop/core-site.xml
 fi
 
-if ["$KEYTAB_PATH" != ""]; then
+if [ "$KEYTAB_PATH" != "" ]; then
 	sed "s/KEYTAB_PATH/$KEYTAB_PATH/" /opt/hadoop/etc/hadoop/core-site.xml >> /opt/hadoop/etc/hadoop/core-site.xml
 fi
 
-if ["$USER_HOME_DIR" != ""]; then
+if [ "$USER_HOME_DIR" != "" ]; then
 	sed "s/USER_HOME_DIR/$USER_HOME_DIR/" /opt/hadoop/etc/hadoop/core-site.xml >> /opt/hadoop/etc/hadoop/core-site.xml
 fi
 
-if ["$CONTAINER_DIR" != ""]; then
+if [ "$CONTAINER_DIR" != "" ]; then
 	cp $CONTAINER_DIR/datalake-1.1-SNAPSHOT.jar $HADOOP_CLASSPATH 
     	cp $CONTAINER_DIR/datalake-1.1-SNAPSHOT.jar $JAVA_CLASSPATH
     	cp $CONTAINER_DIR/.k5keytab $KEYTAB_PATH
